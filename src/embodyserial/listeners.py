6407b38094f3be42705156a55999a91015b5eae7
Espen Westgaard <espenwest@gmail.com>
2022-09-02 10:33:58 +0200

    Fix flake8 warnings

--- src/embodyserial/listeners.py
+++ src/embodyserial/listeners.py
@@ -1,6 +1,7 @@
 """Listener interfaces that can be subscribed to by clients."""
 
 from abc import ABC
+from abc import abstractmethod
 
 from embodycodec import codec
 
@@ -8,6 +9,7 @@ from embodycodec import codec
 class MessageListener(ABC):
     """Listener interface for being notified of incoming messages."""
 
+    @abstractmethod
     def message_received(self, msg: codec.Message) -> None:
         """Process received message"""
         pass
@@ -16,6 +18,7 @@ class MessageListener(ABC):
 class ResponseMessageListener(ABC):
     """Listener interface for being notified of incoming response messages."""
 
+    @abstractmethod
     def response_message_received(self, msg: codec.Message) -> None:
         """Process received response message"""
         pass
@@ -24,6 +27,7 @@ class ResponseMessageListener(ABC):
 class ConnectionListener(ABC):
     """Listener interface for being notified of connection changes."""
 
+    @abstractmethod
     def on_connected(self, connected: bool) -> None:
         """Process connection status."""
         pass
