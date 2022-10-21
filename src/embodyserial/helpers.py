209d99a67b196d97815e0ec8250efe58c6741a5d
Espen Westgaard <espenwest@gmail.com>
2022-10-21 19:27:58 +0200

    Added more getters

--- src/embodyserial/helpers.py
+++ src/embodyserial/helpers.py
@@ -18,11 +18,71 @@ class EmbodySendHelper:
         self.__send_timeout = timeout
 
     def get_current_time(self) -> Optional[datetime]:
+        response_attribute = self.__do_send_get_attribute_request(
+            attributes.CurrentTimeAttribute.attribute_id
+        )
+        return (
+            datetime.fromtimestamp(response_attribute.value / 1000, tz=timezone.utc)
+            if response_attribute
+            else None
+        )
+
+    def get_serial_no(self) -> Optional[str]:
+        response_attribute = self.__do_send_get_attribute_request(
+            attributes.SerialNoAttribute.attribute_id
+        )
+        return response_attribute.formatted_value() if response_attribute else None
+
+    def get_vendor(self) -> Optional[str]:
+        response_attribute = self.__do_send_get_attribute_request(
+            attributes.VendorAttribute.attribute_id
+        )
+        return response_attribute.formatted_value() if response_attribute else None
+
+    def get_model(self) -> Optional[str]:
+        response_attribute = self.__do_send_get_attribute_request(
+            attributes.ModelAttribute.attribute_id
+        )
+        return response_attribute.formatted_value() if response_attribute else None
+
+    def get_bluetooth_mac(self) -> Optional[str]:
+        response_attribute = self.__do_send_get_attribute_request(
+            attributes.BluetoothMacAttribute.attribute_id
+        )
+        return response_attribute.formatted_value() if response_attribute else None
+
+    def get_battery_level(self) -> Optional[int]:
+        response_attribute = self.__do_send_get_attribute_request(
+            attributes.BatteryLevelAttribute.attribute_id
+        )
+        return response_attribute.value if response_attribute else None
+
+    def get_heart_rate(self) -> Optional[int]:
+        response_attribute = self.__do_send_get_attribute_request(
+            attributes.HeartrateAttribute.attribute_id
+        )
+        return response_attribute.value if response_attribute else None
+
+    def get_charge_state(self) -> Optional[bool]:
+        response_attribute = self.__do_send_get_attribute_request(
+            attributes.ChargeStateAttribute.attribute_id
+        )
+        return response_attribute.value if response_attribute else None
+
+    def get_temperature(self) -> Optional[float]:
+        response_attribute = self.__do_send_get_attribute_request(
+            attributes.TemperatureAttribute.attribute_id
+        )
+        return response_attribute.temp_celsius() if response_attribute else None
+
+    def __do_send_get_attribute_request(
+        self, attribute_id: int
+    ) -> Optional[attributes.Attribute]:
         response = self.__sender.send(
-            codec.GetAttribute(attributes.CurrentTimeAttribute.attribute_id),
+            codec.GetAttribute(attribute_id),
             self.__send_timeout,
         )
         if response and isinstance(response, codec.GetAttributeResponse):
-            return datetime.fromtimestamp(response.value.value / 1000, tz=timezone.utc)
+            return response.value
         else:
             return None
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -13,15 +13,7 @@ from embodyserial import helpers
 
 
 def test_get_current_time_success() -> None:
-    sender: helpers.EmbodySender = Mock()
-    sender.send = Mock(
-        return_value=codec.GetAttributeResponse(
-            attribute_id=attributes.CurrentTimeAttribute.attribute_id,
-            changed_at=int(datetime.now().timestamp() * 1000),
-            reporting=types.Reporting(interval=1, on_change=2),
-            value=attributes.CurrentTimeAttribute(1666368870000),
-        )
-    )
+    sender = __create_sender_mock(attr=attributes.CurrentTimeAttribute(1666368870000))
     send_helper = helpers.EmbodySendHelper(sender=sender)
     current_time = send_helper.get_current_time()
     assert current_time
@@ -43,3 +35,24 @@ def test_get_current_time_with_exception() -> None:
     send_helper = helpers.EmbodySendHelper(sender=sender)
     with pytest.raises(Exception):
         send_helper.get_current_time()
+
+
+def test_get_serial_no_success() -> None:
+    sender = __create_sender_mock(attr=attributes.SerialNoAttribute(12345678))
+    send_helper = helpers.EmbodySendHelper(sender=sender)
+    serial_no = send_helper.get_serial_no()
+    assert serial_no
+    assert serial_no == "0000000000bc614e"
+
+
+def __create_sender_mock(attr: attributes.Attribute) -> helpers.EmbodySender:
+    sender: helpers.EmbodySender = Mock()
+    sender.send = Mock(
+        return_value=codec.GetAttributeResponse(
+            attribute_id=attr.attribute_id,
+            changed_at=int(datetime.now().timestamp() * 1000),
+            reporting=types.Reporting(interval=1, on_change=2),
+            value=attr,
+        )
+    )
+    return sender
