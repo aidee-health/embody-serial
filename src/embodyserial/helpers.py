f096b87a6feb1c51b9b193f487841a3e550fe70b
Espen Westgaard <espenwest@gmail.com>
2022-10-21 22:03:15 +0200

    Added more helper methods

--- src/embodyserial/helpers.py
+++ src/embodyserial/helpers.py
@@ -6,6 +6,7 @@ from typing import Optional
 
 from embodycodec import attributes
 from embodycodec import codec
+from embodycodec import types
 
 from embodyserial.embodyserial import EmbodySender
 
@@ -80,6 +81,68 @@ class EmbodySendHelper:
             else None
         )
 
+    def get_firmware_version(self) -> Optional[str]:
+        response_attribute = self.__do_send_get_attribute_request(
+            attributes.FirmwareVersionAttribute.attribute_id
+        )
+        return response_attribute.value if response_attribute else None
+
+    def get_files(self) -> list[str]:
+        response = self.__sender.send(
+            msg=codec.ListFiles(), timeout=self.__send_timeout
+        )
+        files = list()
+        if response and isinstance(response, codec.ListFilesResponse):
+            if len(response.files) == 0:
+                return files
+            else:
+                for file in response.files:
+                    files.append(str(file.file_name))
+        else:
+            return files
+
+    def delete_file(self, file_name: str) -> bool:
+        response = self.__sender.send(
+            msg=codec.DeleteFile(types.File(file_name)), timeout=self.__send_timeout
+        )
+        return (
+            True
+            if response and isinstance(response, codec.DeleteFileResponse)
+            else False
+        )
+
+    def get_file(self, file_name: str) -> bool:
+        response = self.__sender.send(
+            msg=codec.GetFile(file=types.File(file_name=file_name)),
+            timeout=self.__send_timeout,
+        )
+        if not response or not isinstance(response, codec.GetFileResponse):
+            return False
+        # TODO (Espen - 2022-10-21): add listener and act on codec.SendFile message
+        # set path in init or in this method
+        return True
+
+    def set_current_timestamp(self) -> bool:
+        return self.set_timestamp(datetime.now(timezone.utc))
+
+    def set_timestamp(self, time: datetime) -> bool:
+        attr = attributes.CurrentTimeAttribute(int(time.timestamp() * 1000))
+        return self.__do_send_set_attribute_request(attr)
+
+    def set_trace_level(self, level: int) -> bool:
+        attr = attributes.TraceLevelAttribute(level)
+        return self.__do_send_set_attribute_request(attr)
+
+    def reformat_disk(self, file_name: str) -> bool:
+        response = self.__sender.send(
+            msg=codec.ReformatDisk, timeout=self.__send_timeout
+        )
+        return (
+            True
+            if response and isinstance(response, codec.ReformatDiskResponse)
+            else False
+        )
+
     def __do_send_get_attribute_request(
         self, attribute_id: int
     ) -> Optional[attributes.Attribute]:
@@ -90,3 +153,9 @@ class EmbodySendHelper:
             return response.value
         else:
             return None
+
+    def __do_send_set_attribute_request(self, attr: attributes.Attribute) -> bool:
+        response = self.__sender.send(
+            msg=codec.SetAttribute(attr.attribute_id), timeout=self.__send_timeout
+        )
+        return response and isinstance(response, codec.SetAttributeResponse)
