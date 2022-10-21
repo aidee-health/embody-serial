e4d976c6824781ff090811b41c0f777719b9d181
Espen Westgaard <espenwest@gmail.com>
2022-10-21 12:48:44 +0200

    Test and implementation of first helper method for current_time

---
+++ src/embodyserial/helpers.py
@@ -0,0 +1,28 @@
+"""Helpers for the embodyserial interface."""
+
+from datetime import datetime
+from datetime import timezone
+from typing import Optional
+
+from embodycodec import attributes
+from embodycodec import codec
+
+from embodyserial.embodyserial import EmbodySender
+
+
+class EmbodySendHelper:
+    """Facade to make send/receive more protocol agnostic with simple get/set methods."""
+
+    def __init__(self, sender: EmbodySender, timeout: Optional[int] = 30) -> None:
+        self.__sender = sender
+        self.__send_timeout = timeout
+
+    def get_current_time(self) -> Optional[datetime]:
+        response = self.__sender.send(
+            codec.GetAttribute(attributes.CurrentTimeAttribute.attribute_id),
+            self.__send_timeout,
+        )
+        if response and isinstance(response, codec.GetAttributeResponse):
+            return datetime.fromtimestamp(response.value / 1000, tz=timezone.utc)
+        else:
+            return None
---
+++ tests/test_helpers.py
@@ -0,0 +1,5 @@
+"""Test cases for the helpers module."""
+
+
+def test_get_current_time() -> None:
+    """Test successful get current time."""
